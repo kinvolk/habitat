a3cbf87300e3dc322a503cb88430a7f35e5b45b5
Krzesimir Nowak <krzesimir@kinvolk.io>
2017-07-29 00:25:44 +0200

    Add peer seed to butterfly server
    This serves as an another way of getting the initial list of
    peers. The difference is that it can be filled at any time after the
    start. This list is processed only if we didn't have any initial peers
    set (e.g. with the `--peer` flag) and if we still don't have members
    in census.
    
    Also, there is no need to filter ourselves out from ping messages -
    the supervisor seems to handle this case very well. At first I tried
    to make supervisor blacklist itself, but that apparently was too big
    of a hammer, because it broke election.
    
    Signed-off-by: Krzesimir Nowak <krzesimir@kinvolk.io>

--- components/butterfly/src/server/mod.rs
+++ components/butterfly/src/server/mod.rs
@@ -90,6 +90,7 @@ pub struct Server {
     swim_rounds: Arc<AtomicIsize>,
     gossip_rounds: Arc<AtomicIsize>,
     blacklist: Arc<RwLock<HashSet<String>>>,
+    peer_seed: Arc<RwLock<Vec<Member>>>
 }
 
 impl Clone for Server {
@@ -119,6 +120,7 @@ impl Clone for Server {
             gossip_rounds: self.gossip_rounds.clone(),
             blacklist: self.blacklist.clone(),
             socket: None,
+            peer_seed: Arc::clone(&self.peer_seed),
         }
     }
 }
@@ -173,6 +175,7 @@ impl Server {
                     gossip_rounds: Arc::new(AtomicIsize::new(0)),
                     blacklist: Arc::new(RwLock::new(HashSet::new())),
                     socket: None,
+                    peer_seed: Arc::new(RwLock::new(Vec::new())),
                 })
             }
             (Err(e), _) | (_, Err(e)) => Err(Error::CannotBind(e)),
@@ -342,6 +345,24 @@ impl Server {
                 });
         }
 
+        Ok(())
+    }
+
+    pub fn need_peer_seeding(&self) -> bool {
+        if self.member_list.len_initial_members() == 0 {
+            let m = self.member_list.members.read().expect(
+                "Members lock is poisoned",
+            );
+            m.is_empty()
+        } else {
+            false
+        }
+    }
+
+    pub fn set_peer_seed(&mut self, mut members: Vec<Member>) -> Result<()> {
+        let mut peer_seed = self.peer_seed.write().expect("Peer seed lock is poisoned");
+        peer_seed.clear();
+        peer_seed.append(&mut members);
         Ok(())
     }
 
