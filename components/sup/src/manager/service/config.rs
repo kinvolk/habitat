6c5b5e5c009592a2f6df8fe1c901e71356bbaa6f
Lorenzo Manacorda <lorenzo@kinvolk.io>
2017-09-29 17:14:13 +0200

    Add or update some API docs
    Some functions were undocumented, some of them had wrong docs or
    typos, some of them needed a small update in docs after the
    reload-on-configuration-change work.
    
    Signed-off-by: Lorenzo Manacorda <lorenzo@kinvolk.io>

--- components/sup/src/manager/service/config.rs
+++ components/sup/src/manager/service/config.rs
@@ -139,7 +139,7 @@ impl Cfg {
     /// Updates the service configuration with data from a census group if the census group has
     /// newer data than the current configuration.
     ///
-    /// Returns true if the configuration was updated.
+    /// Returns `true` if the configuration was updated.
     pub fn update(&mut self, census_group: &CensusGroup) -> bool {
         match census_group.service_config {
             Some(ref config) => {
@@ -351,6 +351,7 @@ impl Serialize for Cfg {
 }
 
 #[derive(Debug)]
+/// Renders configuration templates into config files.
 pub struct CfgRenderer(TemplateRenderer);
 
 impl CfgRenderer {
@@ -388,6 +389,8 @@ impl CfgRenderer {
     }
 
     /// Compile and write all configuration files to the configuration directory.
+    ///
+    /// Returns `true` if the configuration has changed.
     pub fn compile(&self, pkg: &Pkg, ctx: &RenderContext) -> Result<bool> {
         // JW TODO: This function is loaded with IO errors that will be converted a Supervisor
         // error resulting in the end-user not knowing what the fuck happned at all. We need to go
