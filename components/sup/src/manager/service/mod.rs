kinvolk/habitat

commit a9c98dd435c278c6dda0861cc27f3e900e54d8c7

Add Smoke Test hook and overall hook refactorings
This change adds a Smoke Test hook to the HookTable. It is currently
not executed because further refactorings are necessary to ensure
that hooks are run asynchronously without causing the main loop of
the manager to block.

This commit also contains a number of refactors to move hooks from
the package module in the supervisor crate to the service module.
While hook templates are defined in a package, they are largely a
component of the runtime and should be owned by the service module.

In a future commit we will hook up the smoke test and health check
hooks to background threads which will feed a cache on the service
struct to be read by the gossip server and http-gateway.

Signed-off-by: Jamie Winsor <jamie@vialstudios.com>

components/sup/src/manager/service/mod.rs
@@ -13,6 +13,8 @@
 // limitations under the License.
 
 pub mod config;
+pub mod health;
+pub mod hooks;
 
 use std;
 use std::collections::HashMap;
@@ -30,9 +32,10 @@ use hcore::util::perm::{set_owner, set_permissions};
 use toml;
 
 pub use self::config::ServiceConfig;
+pub use self::health::{HealthCheck, SmokeCheck};
+use self::hooks::{HOOK_PERMISSIONS, HookType};
 use config::gconfig;
 use error::{Error, Result, SupError};
-use health_check;
 use manager::signals;
 use manager::census::CensusList;
 use package::Package;
@@ -90,10 +93,6 @@ impl Service {
         })
     }
 
-    pub fn service_group_str(&self) -> String {
-        format!("{}", self.service_group)
-    }
-
     pub fn start(&mut self) -> Result<()> {
         self.supervisor.start()
     }
@@ -101,20 +100,20 @@ impl Service {
     pub fn restart(&mut self, census_list: &CensusList) -> Result<()> {
         match self.topology {
             Topology::Leader => {
-                if let Some(census) = census_list.get(&self.service_group.to_string()) {
+                if let Some(census) = census_list.get(&*self.service_group) {
                     // We know perfectly well we are in this census, because we asked for
                     // our own service group *by name*
                     let me = census.me().unwrap();
                     if me.get_election_is_running() {
                         if self.last_restart_display != LastRestartDisplay::ElectionInProgress {
-                            outputln!(preamble self.service_group_str(),
+                            outputln!(preamble self.service_group,
                                       "Not restarting service; {}",
                                       Yellow.bold().paint("election in progress."));
                             self.last_restart_display = LastRestartDisplay::ElectionInProgress;
                         }
                     } else if me.get_election_is_no_quorum() {
                         if self.last_restart_display != LastRestartDisplay::ElectionNoQuorum {
-                            outputln!(preamble self.service_group_str(),
+                            outputln!(preamble self.service_group,
                                       "Not restarting service; {}, {}.",
                                       Yellow.bold().paint("election in progress"),
                                       Red.bold().paint("and we have no quorum"));
@@ -124,7 +123,7 @@ impl Service {
                         // We know we have a leader, so this is fine
                         let leader_id = census.get_leader().unwrap().get_member_id();
                         if self.last_restart_display != LastRestartDisplay::ElectionFinished {
-                            outputln!(preamble self.service_group_str(),
+                            outputln!(preamble self.service_group,
                                       "Restarting service; {} is the leader",
                                       Green.bold().paint(leader_id));
                             self.last_restart_display = LastRestartDisplay::ElectionFinished;
@@ -190,53 +189,53 @@ impl Service {
             let mut new_file = match File::create(&new_filename) {
                 Ok(new_file) => new_file,
                 Err(e) => {
-                    outputln!(preamble self.service_group_str(),
-                        "Service file from butterfly failed to open the new file {}: {}",
-                        new_filename,
-                        Red.bold().paint(format!("{}", e)));
+                    outputln!(preamble self.service_group,
+                              "Service file from butterfly failed to open the new file {}: {}",
+                              new_filename,
+                              Red.bold().paint(format!("{}", e)));
                     return false;
                 }
             };
 
             if let Err(e) = new_file.write_all(&body) {
-                outputln!(preamble self.service_group_str(),
-                    "Service file from butterfly failed to write {}: {}",
-                    new_filename,
-                    Red.bold().paint(format!("{}", e)));
+                outputln!(preamble self.service_group,
+                          "Service file from butterfly failed to write {}: {}",
+                          new_filename,
+                          Red.bold().paint(format!("{}", e)));
                 return false;
             }
 
             if let Err(e) = std::fs::rename(&new_filename, &on_disk_path) {
-                outputln!(preamble self.service_group_str(),
-                    "Service file from butterfly failed to rename {} to {}: {}",
-                    new_filename,
-                    on_disk_path.to_string_lossy(),
-                    Red.bold().paint(format!("{}", e)));
+                outputln!(preamble self.service_group,
+                          "Service file from butterfly failed to rename {} to {}: {}",
+                          new_filename,
+                          on_disk_path.to_string_lossy(),
+                          Red.bold().paint(format!("{}", e)));
                 return false;
             }
 
             if let Err(e) = set_owner(&on_disk_path,
                                       &self.supervisor.runtime_config.svc_user,
                                       &self.supervisor.runtime_config.svc_group) {
-                outputln!(preamble self.service_group_str(),
-                    "Service file from butterfly failed to set ownership on {}: {}",
-                    on_disk_path.to_string_lossy(),
-                    Red.bold().paint(format!("{}", e)));
+                outputln!(preamble self.service_group,
+                          "Service file from butterfly failed to set ownership on {}: {}",
+                          on_disk_path.to_string_lossy(),
+                          Red.bold().paint(format!("{}", e)));
                 return false;
             }
 
             if let Err(e) = set_permissions(&on_disk_path, 0o640) {
-                outputln!(preamble self.service_group_str(),
-                    "Service file from butterfly failed to set permissions on {}: {}",
-                    on_disk_path.to_string_lossy(),
-                    Red.bold().paint(format!("{}", e)));
+                outputln!(preamble self.service_group,
+                          "Service file from butterfly failed to set permissions on {}: {}",
+                          on_disk_path.to_string_lossy(),
+                          Red.bold().paint(format!("{}", e)));
                 return false;
             }
 
-            outputln!(preamble self.service_group_str(),
-                "Service file updated from butterfly {}: {}",
-                on_disk_path.to_string_lossy(),
-                Green.bold().paint(new_checksum));
+            outputln!(preamble self.service_group,
+                      "Service file updated from butterfly {}: {}",
+                      on_disk_path.to_string_lossy(),
+                      Green.bold().paint(new_checksum));
             true
         } else {
             false
@@ -264,96 +263,123 @@ impl Service {
             let mut new_file = match File::create(&new_filename) {
                 Ok(new_file) => new_file,
                 Err(e) => {
-                    outputln!(preamble self.service_group_str(),
-                        "Service configuration from butterfly failed to open the new file: {}",
-                        Red.bold().paint(format!("{}", e)));
+                    outputln!(preamble self.service_group,
+                              "Service configuration from butterfly failed to open the new file: \
+                               {}",
+                              Red.bold().paint(format!("{}", e)));
                     return false;
                 }
             };
 
             if let Err(e) = new_file.write_all(encoded.as_bytes()) {
-                outputln!(preamble self.service_group_str(),
-                    "Service configuration from butterfly failed to write: {}",
-                    Red.bold().paint(format!("{}", e)));
+                outputln!(preamble self.service_group,
+                          "Service configuration from butterfly failed to write: {}",
+                          Red.bold().paint(format!("{}", e)));
                 return false;
             }
 
             if let Err(e) = std::fs::rename(&new_filename, &on_disk_path) {
-                outputln!(preamble self.service_group_str(),
-                    "Service configuration from butterfly failed to rename: {}",
-                    Red.bold().paint(format!("{}", e)));
+                outputln!(preamble self.service_group,
+                          "Service configuration from butterfly failed to rename: {}",
+                          Red.bold().paint(format!("{}", e)));
                 return false;
             }
 
             if let Err(e) = set_owner(&on_disk_path,
                                       &self.supervisor.runtime_config.svc_user,
                                       &self.supervisor.runtime_config.svc_group) {
-                outputln!(preamble self.service_group_str(),
-                    "Service configuration from butterfly failed to set ownership: {}",
-                    Red.bold().paint(format!("{}", e)));
+                outputln!(preamble self.service_group,
+                          "Service configuration from butterfly failed to set ownership: {}",
+                          Red.bold().paint(format!("{}", e)));
                 return false;
             }
 
             if let Err(e) = set_permissions(&on_disk_path, 0o640) {
-                outputln!(preamble self.service_group_str(),
-                    "Service configuration from butterfly failed to set permissions: {}",
-                    Red.bold().paint(format!("{}", e)));
+                outputln!(preamble self.service_group,
+                          "Service configuration from butterfly failed to set permissions: {}",
+                          Red.bold().paint(format!("{}", e)));
                 return false;
             }
 
-            outputln!(preamble self.service_group_str(),
-                "Service configuration updated from butterfly: {}",
-                Green.bold().paint(new_checksum));
+            outputln!(preamble self.service_group,
+                      "Service configuration updated from butterfly: {}",
+                      Green.bold().paint(new_checksum));
             true
         } else {
             false
         }
     }
 
-    pub fn health_check(&self) -> Result<health_check::CheckResult> {
-        self.package.health_check(&self.supervisor, &self.service_group)
+    pub fn health_check(&self) -> HealthCheck {
+        if let Some(hook) = self.hooks().health_check {
+            match hook.run(&self.service_group) {
+                Ok(()) => HealthCheck::Ok,
+                Err(SupError { err: Error::HookFailed(_, 1), .. }) => HealthCheck::Warning,
+                Err(SupError { err: Error::HookFailed(_, 2), .. }) => HealthCheck::Critical,
+                Err(SupError { err: Error::HookFailed(_, 3), .. }) => HealthCheck::Unknown,
+                Err(SupError { err: Error::HookFailed(_, code), .. }) => {
+                    outputln!(preamble self.service_group,
+                        "Health check exited with an unknown status code, {}", code);
+                    HealthCheck::Unknown
+                }
+                Err(err) => {
+                    outputln!(preamble self.service_group, "Health check couldn't be run, {}", err);
+                    HealthCheck::Unknown
+                }
+            }
+        } else {
+            match self.supervisor.status() {
+                (true, _) => HealthCheck::Ok,
+                (false, _) => HealthCheck::Critical,
+            }
+        }
     }
 
+    pub fn hooks(&self) -> hooks::HookTable {
+        let mut hooks = hooks::HookTable::new(&self.package, &self.service_group);
+        hooks.load_hooks();
+        hooks
+    }
+
+    /// Run file_updated hook if present
     pub fn file_updated(&self) -> bool {
         if self.initialized {
-            let sg = self.service_group_str();
-            match self.package.file_updated(&self.service_group) {
-                Ok(_) => {
-                    outputln!(preamble sg, "{}", "File update hook succeeded.");
+            match self.hooks().try_run(HookType::FileUpdated) {
+                Ok(()) => {
+                    outputln!(preamble self.service_group, "File update hook succeeded");
                     return true;
                 }
-                Err(e) => outputln!(preamble sg, "File update hook failed: {}", e),
+                Err(err) => {
+                    outputln!(preamble self.service_group, "File update hook failed: {}", err);
+                }
             }
         }
         false
     }
 
+    /// Run initialization hook if present
     pub fn initialize(&mut self) {
         if !self.initialized {
-            match self.package.initialize(&self.service_group) {
-                Ok(()) => {
-                    outputln!(preamble self.service_group_str(), "{}", "Initializing");
-                    self.initialized = true
-                }
-                Err(e) => {
-                    outputln!(preamble self.service_group_str(), "Initialization failed: {}", e)
-                }
+            outputln!(preamble self.service_group, "Initializing");
+            if let Some(err) = self.hooks().try_run(HookType::Init).err() {
+                outputln!(preamble self.service_group, "Initialization failed: {}", err);
+                return;
             }
+            self.initialized = true;
         }
     }
 
     pub fn load_service_config(&self, census: &CensusList) -> Result<ServiceConfig> {
-        ServiceConfig::new(&self.service_group_str(),
-                           &self.package,
-                           census,
-                           gconfig().bind())
+        ServiceConfig::new(&self.service_group, &self.package, census, gconfig().bind())
     }
 
+    /// Run reconfigure hook if present. Return false if it is not present, to trigger default
+    /// restart behavior.
     pub fn reconfigure(&mut self, census_list: &CensusList) -> Option<ServiceConfig> {
         let mut service_config = match self.load_service_config(census_list) {
             Ok(sc) => sc,
             Err(e) => {
-                outputln!(preamble self.service_group_str(),
+                outputln!(preamble self.service_group,
                           "Error generating Service Configuration; not reconfiguring: {}",
                           e);
                 return None;
@@ -362,28 +388,61 @@ impl Service {
         match service_config.write(&self.package) {
             Ok(true) => {
                 self.needs_restart = true;
-                match self.package.reconfigure(&self.service_group) {
-                    Ok(_) => {}
-                    Err(e) => {
-                        outputln!(preamble self.service_group_str(),
-                            "Reconfiguration hook failed: {}", e);
-                    }
+                if let Some(err) = self.hooks().try_run(HookType::Reconfigure).err() {
+                    outputln!(preamble self.service_group,
+                              "Reconfiguration hook failed: {}",
+                              err);
                 }
             }
             Ok(false) => {}
             Err(e) => {
-                outputln!(preamble self.service_group_str(),
-                    "Failed to write service configuration: {}", e);
+                outputln!(preamble self.service_group,
+                          "Failed to write service configuration: {}",
+                          e);
             }
         }
-
-        self.package.hooks().load_hooks();
-        // Probably worth moving the run hook under compile all, eventually
-        if let Err(e) = self.package.copy_run(&service_config) {
-            outputln!("Failed to copy run hook: {}", e);
+        // JW TODO: We probably don't need to reload everything. Just updating them is more
+        // appropriate.
+        self.hooks().compile(&service_config);
+        if let Some(err) = self.copy_run().err() {
+            outputln!(preamble self.service_group, "Failed to copy run hook: {}", err);
         }
-        self.package.hooks().compile_all(&service_config);
         Some(service_config)
+    }
+
+    pub fn smoke_test(&self) -> SmokeCheck {
+        match self.hooks().try_run(HookType::SmokeTest) {
+            Ok(()) => SmokeCheck::Ok,
+            Err(SupError { err: Error::HookFailed(_, code), .. }) => SmokeCheck::Failed(code),
+            Err(err) => {
+                outputln!(preamble self.service_group, "Smoke test couldn't be run, {}", err);
+                SmokeCheck::Failed(-1)
+            }
+        }
+    }
+
+    // Copy the "run" file to the svc path.
+    fn copy_run(&self) -> Result<()> {
+        let svc_run = self.package.pkg_install.svc_path().join(hooks::RUN_FILENAME);
+        match self.hooks().run {
+            Some(hook) => {
+                try!(std::fs::copy(hook.path, &svc_run));
+                try!(set_permissions(&svc_run.to_str().unwrap(), HOOK_PERMISSIONS));
+            }
+            None => {
+                let run = self.package.path().join(hooks::RUN_FILENAME);
+                match std::fs::metadata(&run) {
+                    Ok(_) => {
+                        try!(std::fs::copy(&run, &svc_run));
+                        try!(set_permissions(&svc_run, HOOK_PERMISSIONS));
+                    }
+                    Err(err) => {
+                        outputln!(preamble self.service_group, "Error finding run file: {}", err);
+                    }
+                }
+            }
+        }
+        Ok(())
     }
 }
 

components/sup/src/manager/mod.rs
@@ -336,7 +336,7 @@ impl Manager {
         if let Some((incarnation, config)) =
             self.state
                 .butterfly
-                .service_config_for(&service.service_group_str(), Some(service.cfg_incarnation)) {
+                .service_config_for(&*service.service_group, Some(service.cfg_incarnation)) {
             service.cfg_incarnation = incarnation;
             service.write_butterfly_service_config(config)
         } else {
@@ -352,7 +352,7 @@ impl Manager {
         for (incarnation, filename, body) in
             self.state
                 .butterfly
-                .service_files_for(&service.service_group_str(), &service.current_service_files)
+                .service_files_for(&*service.service_group, &service.current_service_files)
                 .into_iter() {
             if service.write_butterfly_service_file(filename, incarnation, body) {
                 updated = true;

components/sup/src/error.rs
@@ -49,16 +49,16 @@ use std::string;
 use std::sync::mpsc;
 
 use ansi_term::Colour::Red;
-use handlebars;
-use hcore::package::Identifiable;
 use butterfly;
-use toml;
-
 use common;
 use depot_client;
+use handlebars;
 use hcore::{self, package};
+use hcore::package::Identifiable;
+use toml;
+
+use manager::service::hooks::HookType;
 use output::StructuredOutput;
-use package::HookType;
 use PROGRAM_NAME;
 
 static LOGKEY: &'static str = "ER";
@@ -110,8 +110,6 @@ pub enum Error {
     HabitatCore(hcore::Error),
     TemplateFileError(handlebars::TemplateFileError),
     TemplateRenderError(handlebars::RenderError),
-    /// Health check exited with an invalid status code. Valid status codes are 0, 1, 2, and 3.
-    HealthCheckBadExit(i32),
     /// A hook failed to successfully execute. This error contains the type of hook which failed
     /// to run and the exit code.
     HookFailed(HookType, i32),
@@ -165,9 +163,6 @@ impl fmt::Display for SupError {
             Error::DepotClient(ref err) => format!("{}", err),
             Error::EnvJoinPathsError(ref err) => format!("{}", err),
             Error::FileNotFound(ref e) => format!("File not found at: {}", e),
-            Error::HealthCheckBadExit(ref e) => {
-                format!("Health check exited with an unknown status code, {}", e)
-            }
             Error::HookFailed(ref hook, ref code) => {
                 format!("{} hook failed to run with exit code {}", hook, code)
             }
@@ -259,7 +254,6 @@ impl error::Error for SupError {
             Error::DepotClient(ref err) => err.description(),
             Error::EnvJoinPathsError(ref err) => err.description(),
             Error::FileNotFound(_) => "File not found",
-            Error::HealthCheckBadExit(_) => "Health Check exited with an unknown status code",
             Error::HookFailed(_, _) => "Hook failed to run",
             Error::InvalidBinding(_) => "Invalid binding parameter",
             Error::InvalidKeyParameter(_) => "Key parameter error",

components/sup/src/health_check.rs
@@ -1,35 +1,0 @@
-// Copyright (c) 2016-2017 Chef Software Inc. and/or applicable contributors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-use std::fmt::{self, Display, Formatter};
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub enum CheckResult {
-    Ok,
-    Warning,
-    Critical,
-    Unknown,
-}
-
-impl Display for CheckResult {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        let msg = match *self {
-            CheckResult::Ok => "OK",
-            CheckResult::Warning => "WARNING",
-            CheckResult::Critical => "CRITICAL",
-            CheckResult::Unknown => "UNKNOWN",
-        };
-        write!(f, "{}", msg)
-    }
-}

components/sup/src/http_gateway.rs
@@ -32,11 +32,24 @@ use prometheus;
 
 use config::gconfig;
 use error::{Result, Error, SupError};
-use health_check;
 use manager;
+use manager::service::HealthCheck;
 
 static LOGKEY: &'static str = "HG";
 
+// Simple macro to encapsulate the HTTP metrics for each endpoint
+macro_rules! with_metrics {
+    ($method:expr, $name:expr) => {{
+        let mut labels = HashMap::new();
+        labels.insert("handler", $name);
+
+        HTTP_COUNTER.with(&labels.clone()).inc();
+        let timer = HTTP_REQ_HISTOGRAM.with(&labels).start_timer();
+        let result = $method;
+        timer.observe_duration();
+        result
+    }}
+}
 
 lazy_static! {
     static ref HTTP_COUNTER: CounterVec = register_counter_vec!(
@@ -111,21 +124,6 @@ impl typemap::Key for ManagerState {
 
 pub struct Server(Iron<Chain>);
 
-
-// Simple macro to encapsulate the HTTP metrics for each endpoint
-macro_rules! with_metrics {
-    ($method:expr, $name:expr) => {{
-        let mut labels = HashMap::new();
-        labels.insert("handler", $name);
-        
-        HTTP_COUNTER.with(&labels.clone()).inc();
-        let timer = HTTP_REQ_HISTOGRAM.with(&labels).start_timer();
-        let result = $method;
-        timer.observe_duration();
-        result
-    }}
-}
-
 impl Server {
     pub fn new(manager_state: manager::State) -> Self {
         let router = router!(
@@ -133,10 +131,16 @@ impl Server {
             census: get "/census" => with_metrics!(census, "census"),
             metrics: get "/metrics" => with_metrics!(metrics, "metrics"),
             services: get "/services" => with_metrics!(services, "services"),
-            service_config: get "/services/:svc/:group/config" => with_metrics!(config, "config"),
+            service_config: get "/services/:svc/:group/config" => {
+                with_metrics!(config, "config")
+            },
             service_health: get "/services/:svc/:group/health" => with_metrics!(health, "health"),
-            service_config_org: get "/services/:svc/:group/:org/config" => with_metrics!(config, "config"),
-            service_health_org: get "/services/:svc/:group/:org/health" => with_metrics!(health, "config"),
+            service_config_org: get "/services/:svc/:group/:org/config" => {
+                with_metrics!(config, "config")
+            },
+            service_health_org: get "/services/:svc/:group/:org/health" => {
+                with_metrics!(health, "config")
+            }
         );
         let mut chain = Chain::new(router);
         chain.link(persistent::Read::<ManagerState>::both(manager_state));
@@ -154,7 +158,6 @@ impl Server {
         Ok(handle)
     }
 }
-
 
 fn butterfly(req: &mut Request) -> IronResult<Response> {
     let state = req.get::<persistent::Read<ManagerState>>().unwrap();
@@ -196,15 +199,7 @@ fn health(req: &mut Request) -> IronResult<Response> {
     };
     let services = state.services.read().unwrap();
     match services.iter().find(|s| s.service_group == service_group) {
-        Some(service) => {
-            match service.health_check() {
-                Ok(result) => Ok(result.into()),
-                Err(err) => {
-                    error!("Health Check failed, err={:?}", err);
-                    Ok(Response::with(status::InternalServerError))
-                }
-            }
-        }
+        Some(service) => Ok(service.health_check().into()),
         None => Ok(Response::with(status::NotFound)),
     }
 }
@@ -224,20 +219,19 @@ fn metrics(_req: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, String::from_utf8(buffer).unwrap())))
 }
 
-impl Into<Response> for health_check::CheckResult {
+impl Into<Response> for HealthCheck {
     fn into(self) -> Response {
         let status: status::Status = self.into();
         Response::with(status)
     }
 }
 
-impl Into<status::Status> for health_check::CheckResult {
+impl Into<status::Status> for HealthCheck {
     fn into(self) -> status::Status {
         match self {
-            health_check::CheckResult::Ok |
-            health_check::CheckResult::Warning => status::Ok,
-            health_check::CheckResult::Critical => status::ServiceUnavailable,
-            health_check::CheckResult::Unknown => status::InternalServerError,
+            HealthCheck::Ok | HealthCheck::Warning => status::Ok,
+            HealthCheck::Critical => status::ServiceUnavailable,
+            HealthCheck::Unknown => status::InternalServerError,
         }
     }
 }

commit 0993eeb3d2cd7ad0019b8304f06be35c0de4a530

Remove incorrect comment for function
The comment was introduced in a9c98dd435c278c6dda0861cc27f3e900e54d8c7
and was incorrect back then as well. In the meantime, the function has been
changed and now doesn't return any value.

components/sup/src/manager/service/mod.rs
@@ -480,8 +480,7 @@ impl Service {
         }
     }
 
-    /// Run reconfigure hook if present. Return false if it is not present, to trigger default
-    /// restart behavior.
+    /// Run reconfigure hook if present.
     fn reconfigure(&mut self) {
         self.needs_reconfiguration = false;
         if let Some(ref hook) = self.hooks.reconfigure {