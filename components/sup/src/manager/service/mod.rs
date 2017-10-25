a416de075d006ea41befad6754c4da020252762d
Lorenzo Manacorda <lorenzo@kinvolk.io>
2017-10-25 15:33:14 +0200

    Refactor update_templates method
    This refactor aims at simplifying the flow of the code and reducing
    conditionals.
    
    Signed-off-by: Lorenzo Manacorda <lorenzo@kinvolk.io>

--- components/sup/src/manager/service/mod.rs
+++ components/sup/src/manager/service/mod.rs
@@ -418,35 +418,33 @@ impl Service {
             "Service update failed; unable to find own service group",
         );
         let cfg_updated_from_rumors = self.cfg.update(census_group);
-        let cfg_changed = cfg_updated_from_rumors || self.user_config_updated;
-        let something_to_do = cfg_changed || census_ring.changed();
 
-        if something_to_do {
-            if self.user_config_updated {
-                if let Err(e) = self.cfg.load_user(&self.pkg) {
-                    outputln!(preamble self.service_group, "Loading user-config failed: {}", e);
-                }
+
+        if self.user_config_updated {
+            if let Err(e) = self.cfg.load_user(&self.pkg) {
+                outputln!(preamble self.service_group, "Loading user-config failed: {}", e);
             }
-
-            let (reload, reconfigure) = {
-                let ctx = self.render_context(census_ring);
-
-                let hooks_changed = if cfg_updated_from_rumors || census_ring.changed() {
-                    self.compile_hooks(&ctx)
-                } else {
-                    false
-                };
-
-                let reconfigure = self.compile_configuration(&ctx);
-                let reload = hooks_changed || self.user_config_updated;
-
-                (reload, reconfigure)
-            };
-
-            self.needs_reload = reload;
-            self.needs_reconfiguration = reconfigure;
-            self.user_config_updated = false;
         }
+
+        let (cfg_changed, reload, reconfigure) = {
+            let ctx = self.render_context(census_ring);
+
+            if cfg_updated_from_rumors || census_ring.changed() {
+                let config_changed = cfg_updated_from_rumors || self.user_config_updated;
+                let needs_reload = self.compile_hooks(&ctx) || self.user_config_updated;
+                let needs_reconfigure = self.compile_configuration(&ctx);
+
+                (config_changed, needs_reload, needs_reconfigure)
+            } else if self.user_config_updated {
+                (true, true, self.compile_configuration(&ctx))
+            } else {
+                (false, self.needs_reload, self.needs_reconfiguration)
+            }
+        };
+
+        self.needs_reload = reload;
+        self.needs_reconfiguration = reconfigure;
+        self.user_config_updated = false;
 
         cfg_changed
     }
